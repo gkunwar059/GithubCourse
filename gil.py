c62052d9421f9f25dc553194682037722330d945
Ganesh <gjkkunwar07@gmail.com>
2024-01-25 10:12:41 +0545

    second commit

--- gil.py
+++ gil.py
@@ -10,3 +10,16 @@ thread=threading.Thread(target=print_numbers)
 thread1=threading.Thread(target=print_numbers)
 thread.start()
 thread1.start()
+# GIL 
+import threading
+import time
+
+def print_numbers():
+    for i in range(4):
+        print(i)
+        # time.sleep(1)
+
+thread=threading.Thread(target=print_numbers)
+thread1=threading.Thread(target=print_numbers)
+thread.start()
+thread1.start()
